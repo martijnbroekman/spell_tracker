8fd20a4c166bc1ba3045258fa1da4d84018cb25e
Martijn <martijn.oisterwijk@gmail.com>
2026-04-12 22:01:34 +0200

    order by name

--- lib/providers/spells_provider.dart
+++ lib/providers/spells_provider.dart
@@ -5,5 +5,7 @@ import 'package:spell_tracker/providers/sources_provider.dart';
 /// Provides a flat list of all [Spell]s across every loaded [Source].
 final spellsProvider = FutureProvider<List<Spell>>((ref) async {
   final sources = await ref.watch(sourcesProvider.future);
-  return sources.expand((source) => source.spells).toList();
+  final spells = sources.expand((source) => source.spells).toList();
+  spells.sort((a, b) => a.name.compareTo(b.name));
+  return spells;
 });
